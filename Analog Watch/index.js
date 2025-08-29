75e483e59716ce8f85ac09e40834de7f18c66e03
Aabid Husain <mraabid01@gmail.com>
2025-08-29 11:41:08 +0530

    Create index.js

---
+++ Analog Watch/index.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clock By Aabid Husain</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="clock">
+
+            <div id="hour" class="hand hour-width" style="--clr: #f30b2a;"><i></i></div>
+            <div id="min" class="hand min-width" style="--clr: #027aba;"><i></i></div>
+            <div id="sec" class="hand sec-width" style="--clr: #30dc00;"><i></i></div>
+
+            <span style="--i: 1;"><b>1</b></span>
+            <span style="--i: 2;"><b>2</b></span>
+            <span style="--i: 3;"><b>3</b></span>
+            <span style="--i: 4;"><b>4</b></span>
+            <span style="--i: 5;"><b>5</b></span>
+            <span style="--i: 6;"><b>6</b></span>
+            <span style="--i: 7;"><b>7</b></span>
+            <span style="--i: 8;"><b>8</b></span>
+            <span style="--i: 9;"><b>9</b></span>
+            <span style="--i: 10;"><b>10</b></span>
+            <span style="--i: 11;"><b>11</b></span>
+            <span style="--i: 12;"><b>12</b></span>
+        </div>
+    </div>
+
+    <div class="digital-clock">
+        <p id="digital-hour">00</p>
+    </div>
+
+    <div class="copyright">
+      <p>BaniDeveloper</p>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
